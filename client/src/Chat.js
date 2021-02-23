5c228e850b46f6d3a4d4af8ea172a043e167690d
Alex Orr <alex.orr@sendbird.com>
2021-02-23 05:10:36 -0800

    split out chat component

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,7 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { App as SendbirdApp } from 'sendbird-uikit';
+import {
+  Channel,
+  ChannelList,
+  ChannelSettings,
+  SendBirdProvider,
+} from 'sendbird-uikit';
 import 'sendbird-uikit/dist/index.css';
+
+import { useHistory } from 'react-router-dom';
 import { useE3 } from './utils/e3';
 
 const Chat = ({
@@ -17,6 +24,8 @@ const Chat = ({
       history.push('/');
     }
   }, [userId, nickname, history]);
+  const [showSettings, setShowSettings] = useState(false);
+  const [currentChannel, setCurrentChannel] = useState(null);
 
   const { initE3, isInitialized, registerUser } = useE3();
 
@@ -30,12 +39,34 @@ const Chat = ({
   }, [isInitialized, didRegister]);
 
   return isInitialized ? (
-    <SendbirdApp
+    <SendBirdProvider
       appId='7A493E5B-B92F-4D01-AD41-7F568AA2AECA'
       userId={userId}
       nickname={nickname}
       accessToken={accessToken}
-    />
+    >
+      <div className='sendbird-app__wrap'>
+        <div className='sendbird-app__channellist-wrap'>
+          <ChannelList
+            onChannelSelect={(channel) => setCurrentChannel(channel.url)}
+          />
+        </div>
+        <div className='sendbird-app__conversation-wrap'>
+          <Channel
+            channelUrl={currentChannel}
+            onChatHeaderActionClick={() => setShowSettings(true)}
+          />
+        </div>
+      </div>
+      {showSettings && (
+        <div className='sendbird-app__settingspanel-wrap'>
+          <ChannelSettings
+            channelUrl={currentChannel?.url}
+            onCloseClick={() => setShowSettings(false)}
+          />
+        </div>
+      )}
+    </SendBirdProvider>
   ) : (
     <div>Waiting to init E3Kit</div>
   );
