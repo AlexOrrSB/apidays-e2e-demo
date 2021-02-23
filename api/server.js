96ee75fcdb62b23142037fd5a469ff094f64aacf
Alex Orr <alex.orr@sendbird.com>
2021-02-23 02:39:06 -0800

    return virgil jwt token from api

--- api/server.js
+++ api/server.js
@@ -7,6 +7,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
+const {
+  initCrypto,
+  VirgilCrypto,
+  VirgilAccessTokenSigner,
+} = require('virgil-crypto');
+const { JwtGenerator } = require('virgil-sdk');
+
 // Sendbird Constants
 const sendbirdAppId = process.env.SENDBIRD_APP_ID;
 const sendbirdApiToken = process.env.SENDBIRD_API_TOKEN;
@@ -22,7 +29,9 @@ app.post('/login', bodyParser.json(), async (req, res) => {
   const { userId } = body;
   const sendbirdUser = await getSendbirdUser(userId);
   const sendbirdAccessToken = sendbirdUser.access_token;
-  res.status(200).json({ sendbirdAccessToken });
+  const generator = await generatorPromise;
+  const virgilJwtToken = generator.generateToken(userId);
+  res.status(200).json({ sendbirdAccessToken, virgilToken: virgilJwtToken.toString() });
 });
 
 const getSendbirdUser = async (userId) => {
@@ -38,7 +47,9 @@ app.post('/register', bodyParser.json(), async (req, res) => {
   const { userId, nickname } = body;
   const sendbirdUser = await createSendbirdUser(userId, nickname);
   const sendbirdAccessToken = sendbirdUser.access_token;
-  res.status(200).json({ sendbirdAccessToken });
+  const generator = await generatorPromise;
+  const virgilJwtToken = generator.generateToken(userId);
+  res.status(200).json({ sendbirdAccessToken, virgilToken: virgilJwtToken.toString() });
 });
 
 const createSendbirdUser = async (userId, nickname) => {
@@ -54,6 +65,22 @@ const createSendbirdUser = async (userId, nickname) => {
   });
   return await response.json();
 };
+
+// Virgil JWT Generator
+const getJwtGenerator = async () => {
+  await initCrypto();
+
+  const virgilCrypto = new VirgilCrypto();
+
+  return new JwtGenerator({
+    appId: process.env.VIRGIL_APP_ID,
+    apiKeyId: process.env.VIRGIL_APP_KEY_ID,
+    apiKey: virgilCrypto.importPrivateKey(process.env.VIRGIL_APP_KEY),
+    accessTokenSigner: new VirgilAccessTokenSigner(virgilCrypto),
+  });
+};
+
+const generatorPromise = getJwtGenerator();
 
 app.listen(port, () => {
   console.log(`API server started on port ${port}`);
